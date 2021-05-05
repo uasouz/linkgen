89a00a9c4c8333dce614c260bef1cea68adaa01e
hadara <vlopes45@gmail.com>
2021-05-04 21:22:33 -0400

    :sparkles: :bulb: Implements basic HTTP Server,add a few comments and start services.

--- api/server.go
+++ api/server.go
@@ -1,5 +1,30 @@
 package api
 
-type Server struct {
+import (
+	"net/http"
+	"os"
+)
 
+type Server struct {
+	Port     string
+	shutdown chan os.Signal
+}
+
+func (s *Server) Start() {
+	http.ListenAndServe(":"+s.Port, nil)
+
+	//<-s.shutdown
+	// gracefull shutdown
+}
+
+func (s *Server) Stop() {
+	s.shutdown <- os.Kill
+}
+
+func (s *Server) Healthz() {
+
+}
+
+func New(port string) *Server {
+	return &Server{Port: port}
 }
--- cmd/linkgen/main.go
+++ cmd/linkgen/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"linkgen/api"
 	"linkgen/config"
 )
 
 func main() {
+	// Start configuration structure with default values
 	cfg := config.Config{APIPort: "3000"}
 
+	// Call configuration load function
 	cfg.LoadConfig("config.yaml")
 
+	// Creates a new API Service with the port configuration
+	apiService := api.New(cfg.APIPort)
+
+	// Start API Service
+	apiService.Start()
 }
